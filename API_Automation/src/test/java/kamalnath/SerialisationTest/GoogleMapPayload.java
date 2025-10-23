baedec89abb44f6381ca6a32db1d0f5feb05bf62
Kamalnath <kamalnath121998@gmail.vom>
2025-10-23 13:12:42 +0530

    Serialisation of input payload using google map API

---
+++ API_Automation/src/test/java/kamalnath/SerialisationTest/GoogleMapPayload.java
@@ -0,0 +1,24 @@
+package kamalnath.SerialisationTest;
+
+public class GoogleMapPayload {
+	public static String GoogleMapsAddPlacePayload() {
+		return "{\r\n"
+				+ "&quot;location&quot;: {\r\n"
+				+ "&quot;lat&quot;: -38.383494,\r\n"
+				+ "&quot;lng&quot;: 33.427362\r\n"
+				+ "},\r\n"
+				+ "&quot;accuracy&quot;: 50,\r\n"
+				+ "\r\n"
+				+ "&quot;name&quot;: &quot;Frontline house&quot;,\r\n"
+				+ "&quot;phone_number&quot;: &quot;(+91) 983 893 3937&quot;,\r\n"
+				+ "&quot;address&quot;: &quot;29, side layout, cohen 09&quot;,\r\n"
+				+ "&quot;types&quot;: [\r\n"
+				+ "&quot;shoe park&quot;,\r\n"
+				+ "&quot;shop&quot;\r\n"
+				+ "],\r\n"
+				+ "&quot;website&quot;: &quot;http://google.com&quot;,\r\n"
+				+ "&quot;language&quot;: &quot;French-IN&quot;\r\n"
+				+ "}";
+	}
+
+}
